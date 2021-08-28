a04d3be56c91da7e3392710928e36bd05a3bb058
jh59 <29499454+jh59@users.noreply.github.com>
2021-08-28 17:13:48 +0200

    feat: processFileData for weather challenge

--- src/main/java/de/exxcellent/challenge/processing/FileProcessor.java
+++ src/main/java/de/exxcellent/challenge/processing/FileProcessor.java
@@ -17,14 +17,27 @@ public class FileProcessor implements FileProcessorComponent{
      */
     public FileProcessor(List<List<String>> weatherData){
         processedObjects = new ArrayList<>();
+        processFileData(weatherData);
     }
 
     public List<Weather> getProcessedObjects() {
         return processedObjects;
     }
 
+    /**
+     * Process the weather files content to a list of Weather objects.
+     * @param fileContent file content as a list of objects
+     */
     @Override
     public void processFileData(List<List<String>> fileContent) {
-
+        for (List<String> data:
+        fileContent){
+            if(fileContent.indexOf(data) != 0){
+                int day = Integer.parseInt(data.get(0));
+                int maxTemp = Integer.parseInt(data.get(1));
+                int minTemp = Integer.parseInt(data.get(2));
+                processedObjects.add(new Weather(day, maxTemp, minTemp));
+            }
+        }
     }
 }
